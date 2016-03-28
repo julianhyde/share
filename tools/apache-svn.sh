c356d4d99b53f6d2fcc8038263ed3fc08ddb71f1
Julian Hyde <jhyde@apache.org>
2016-03-28 12:21:37 -0700

    Parallelize apache-svn

--- tools/apache-svn.sh
+++ tools/apache-svn.sh
@@ -4,24 +4,28 @@
 # interested in.
 
 function svn-co() {
+  (
     svn co $1 $2
     (cd $2; svn up)
+  ) 2>&1 | sed -e "s!^!$2: !"
 }
 
 mkdir -p ~/apache/private/foundation
 
-svn-co https://svn.apache.org/repos/private/foundation/board ~/apache/private/foundation/board
-svn-co https://svn.apache.org/repos/private/foundation/officers ~/apache/private/foundation/officers
-svn-co https://svn.apache.org/repos/private/committers/board ~/apache/private/committers/board
-svn-co https://svn.apache.org/repos/private/committers/info ~/apache/private/committers/info
+svn-co https://svn.apache.org/repos/private/foundation/board ~/apache/private/foundation/board &
+svn-co https://svn.apache.org/repos/private/foundation/officers ~/apache/private/foundation/officers &
+svn-co https://svn.apache.org/repos/private/committers/board ~/apache/private/committers/board &
+svn-co https://svn.apache.org/repos/private/committers/info ~/apache/private/committers/info &
 
-svn co https://svn.apache.org/repos/asf/incubator/public/trunk ~/apache/asf/incubator
-svn co https://svn.apache.org/repos/asf/infrastructure/site/trunk ~/apache/asf/infrastructure/site
+svn co https://svn.apache.org/repos/asf/incubator/public/trunk ~/apache/asf/incubator &
+svn co https://svn.apache.org/repos/asf/infrastructure/site/trunk ~/apache/asf/infrastructure/site &
 
-for i in kylin incubator/concerted incubator/eagle calcite # drill
+for i in kylin incubator/eagle calcite # drill incubator/concerted
 do
-  svn-co https://dist.apache.org/repos/dist/dev/$i ~/apache/dist/dev/$i
-  svn-co https://dist.apache.org/repos/dist/release/$i ~/apache/dist/release/$i
+  svn-co https://dist.apache.org/repos/dist/dev/$i ~/apache/dist/dev/$i &
+  svn-co https://dist.apache.org/repos/dist/release/$i ~/apache/dist/release/$i &
 done
+
+wait
 
 # End apache-svn.sh
