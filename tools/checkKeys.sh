d5391aebe72f167c2054bd6e6c9d59241b5aff0b
Julian Hyde <jhyde@apache.org>
2016-04-27 13:50:17 -0700

    Fix checkFile on OS X

--- tools/checkKeys.sh
+++ tools/checkKeys.sh
@@ -6,20 +6,20 @@ function doCheckSum() {
   case "${1}.$(uname -s)" in
   (md5.Linux) md5sum "$2" | awk '{print $1}';;
   (sha1.Linux) sha1sum "$2" | awk '{print $1}';;
-  (md5.Darwin) sha1 -q "$2";;
-  (sha1.Darwin) md5 -q "$2";;
+  (md5.Darwin) md5 -q "$2";;
+  (sha1.Darwin) shasum -a 1 "$2" | awk '{print $1}';;
   (*) echo "Unknown command $1 or platform $(uname -s)" >&1; exit 1;;
   esac
 }
 
 function checkHash() {
   cd "$1"
-  for i in *.{zip,pom,gz}; do
+  for i in *.{zip,pom,gz,jar}; do
     if [ ! -f $i ]; then
       continue
     fi
     if [ -f $i.md5 ]; then
-      left="$(cat $i.md5)"
+      left="$(cat $i.md5 | awk '{print $NF}')"
       right="$(doCheckSum md5 $i)"
       if [ "$left" = "$right" ]; then
         echo "$i.md5 present and correct"
@@ -27,12 +27,12 @@ function checkHash() {
         echo "$i.md5 does not match (left: $left, right: $right)"
       fi
     else
-      md5 -q $i > $i.md5
+      doCheckSum md5 $i > $i.md5
       echo $i.md5 created
     fi
 
     if [ -f $i.sha1 ]; then
-      left="$(cat $i.sha1)"
+      left="$(cat $i.sha1 | awk '{print $NF}')"
       right="$(doCheckSum sha1 $i)"
       if [ "$left" = "$right" ]; then
         echo $i.sha1 present and correct
@@ -40,7 +40,7 @@ function checkHash() {
         echo "$i.sha1 does not match (left: $left, right: $right)"
       fi
     else
-      sha1 -q $i > $i.sha1
+      doCheckSum sha1 $i > $i.sha1
       echo $i.sha1 created
     fi
 
