8ad57e460bad833fe7fe317ac771e71385a60454
Julian Hyde <jhyde@apache.org>
2016-09-28 17:56:56 -0700

    Re-organize add-remotes.sh; check for newly added long lines

--- tools/add-remotes.sh
+++ tools/add-remotes.sh
@@ -3,64 +3,84 @@
 #
 # Create as follows:
 #   git remote -v |awk '{printf "git remote add %s %s\n", $1, $2}' | sort -u
+function foo {
+  if test "$3"; then
+    git remote add $2 git@github.com:$2/$3.git
+  else
+    git remote add $2 git@github.com:$2/$1.git
+  fi
+}
+
 case "$1" in
+(sqlline)
+  foo sqlline fineo-io
+  ;;
+
 (calcite)
-  git remote add gparai https://github.com/gparai/calcite.git
-  git remote add aertoria https://github.com/aertoria/incubator-calcite.git
-  git remote add aleph-zero git@github.com:aleph-zero/incubator-calcite.git
-  git remote add amansinha100 git@github.com:amansinha100/incubator-calcite.git
-  git remote add amoghmargoor git@github.com:amoghmargoor/incubator-calcite.git
-  git remote add apache git@github.com:apache/incubator-calcite.git
-  git remote add arina-ielchiieva git@github.com:arina-ielchiieva/calcite.git
-  git remote add bdumon git@github.com:bdumon/optiq.git
-  git remote add benoyantony git@github.com:benoyantony/incubator-optiq.git
-  git remote add d4nc00per git@github.com:d4nc00per/incubator-calcite.git
-  git remote add devth git@github.com:devth/incubator-calcite.git
-  git remote add dingguitao git@github.com:dingguitao/incubator-calcite.git
-  git remote add dremio https://github.com/dremio/calcite.git
-  git remote add ebastien https://github.com/ebastien/calcite.git
-  git remote add gabrielreid git@github.com:gabrielreid/optiq.git
-  git remote add gparai https://github.com/gparai/calcite.git
-  git remote add hortonworks git@github.com:hortonworks/calcite.git
-  git remote add hsuanyi git@github.com:hsuanyi/incubator-calcite.git
-  git remote add jacques-n git@github.com:jacques-n/incubator-calcite.git
-  git remote add jaltekruse git@github.com:jaltekruse/calcite.git
-  git remote add JasonMing git@github.com:JasonMing/incubator-calcite.git
-  git remote add jcamachor git@github.com:jcamachor/calcite.git
-  git remote add jinfengni git@github.com:jinfengni/incubator-optiq.git
-  git remote add joshelser git@github.com:joshelser/incubator-calcite.git
-  git remote add jpullokk https://github.com/jpullokkaran/calcite.git
-  git remote add julianhyde git@github.com:julianhyde/calcite.git
-  git remote add jxiang https://github.com/jxiang/calcite.git
-  git remote add lalinsky git@github.com:lalinsky/incubator-calcite.git
-  git remote add mapr git@github.com:mapr/incubator-calcite.git
-  git remote add maryannxue git@github.com:maryannxue/calcite.git
-  git remote add michaelmior git@github.com:michaelmior/calcite.git
-  git remote add mikehinchey https://github.com/mikehinchey/calcite.git
-  git remote add milinda https://github.com/milinda/calcite.git
-  git remote add mindcrime https://github.com/mindcrime/calcite.git
-  git remote add minji-kim git@github.com:minji-kim/calcite.git
-  git remote add mprudhom https://github.com/mprudhom/incubator-optiq.git
-  git remote add navis git@github.com:navis/incubator-calcite.git
-  git remote add ndimiduk git@github.com:ndimiduk/incubator-calcite.git
+  git remote remove jpullok
   git remote add origin https://git-wip-us.apache.org/repos/asf/calcite.git
-  git remote add rmetzger git@github.com:rmetzger/optiq.git
-  git remote add sbcd90 https://github.com/sbcd90/calcite.git
-  git remote add smola https://github.com/smola/incubator-calcite.git
-  git remote add sreev git@github.com:sreev/incubator-optiq.git
-  git remote add sudheeshkatkam git@github.com:sudheeshkatkam/incubator-calcite.git
-  git remote add tedxu git@github.com:tedxu/calcite.git
-  git remote add vkorukanti git@github.com:vkorukanti/calcite.git
-  git remote add vlsi git@github.com:vlsi/incubator-calcite.git
-  git remote add vrajat git@github.com:vrajat/incubator-calcite.git
-  git remote add wgorman git@github.com:wgorman/optiq.git
-  git remote add xhoong git@github.com:xhoong/incubator-calcite.git
-  git remote add yeongwei git@github.com:yeongwei/incubator-calcite.git
-  git remote add yiming187 https://github.com/yiming187/calcite.git
-  git remote add yixinglu https://github.com/yixinglu/calcite.git
-  git remote add yssharma git@github.com:yssharma/incubator-optiq.git
-  git remote add zhifac git@github.com:zhifac/calcite.git
-  git remote add zinking git@github.com:zinking/calcite.git
+  git remote add ledem git@github.com:julienledem/calcite.git
+
+  foo calcite aertoria incubator-calcite
+  foo calcite aleph-zero incubator-calcite
+  foo calcite amansinha100 incubator-calcite
+  foo calcite amoghmargoor incubator-calcite
+  foo calcite apache incubator-calcite
+  foo calcite arina-ielchiieva
+  foo calcite bdumon optiq
+  foo calcite benoyantony incubator-optiq
+  foo calcite bluejoe2008
+  foo calcite d4nc00per incubator-calcite
+  foo calcite devth incubator-calcite
+  foo calcite dingguitao incubator-calcite
+  foo calcite dremio
+  foo calcite ebastien
+  foo calcite F21
+  foo calcite gabrielreid optiq
+  foo calcite gianm
+  foo calcite gparai
+  foo calcite HeartSaVioR
+  foo calcite hortonworks
+  foo calcite hsuanyi incubator-calcite
+  foo calcite jacques-n incubator-calcite
+  foo calcite jaltekruse
+  foo calcite JasonMing incubator-calcite
+  foo calcite jcamachor
+  foo calcite jinfengni incubator-optiq
+  foo calcite joshelser incubator-calcite
+  foo calcite jpullokkaran
+  foo calcite julianhyde
+  foo calcite jxiang
+  foo calcite lalinsky incubator-calcite
+  foo calcite mapr incubator-calcite
+  foo calcite maryannxue
+  foo calcite michaelmior
+  foo calcite mikehinchey
+  foo calcite milinda
+  foo calcite mindcrime
+  foo calcite minji-kim
+  foo calcite mprudhom incubator-optiq
+  foo calcite navis incubator-calcite
+  foo calcite ndimiduk incubator-calcite
+  foo calcite remerge
+  foo calcite rmetzger optiq
+  foo calcite sbcd90
+  foo calcite smola incubator-calcite
+  foo calcite sreev incubator-optiq
+  foo calcite sudheeshkatkam incubator-calcite
+  foo calcite tedxu
+  foo calcite vkorukanti
+  foo calcite vlsi incubator-calcite
+  foo calcite vrajat incubator-calcite
+  foo calcite wgorman optiq
+  foo calcite wuchong
+  foo calcite xhoong incubator-calcite
+  foo calcite yeongwei incubator-calcite
+  foo calcite yiming187
+  foo calcite yixinglu
+  foo calcite yssharma incubator-optiq
+  foo calcite zhifac
+  foo calcite zinking
   ;;
 esac
 
--- tools/extra.sh
+++ tools/extra.sh
@@ -2,4 +2,9 @@
 case $(pwd) in
 (*/hive/*) exit;;
 esac
+git diff |
+awk '
+/^diff / {f=$3}
+/^\+/ && !/^\+\+\+/ && f ~ /.java/ && length($0) > 81 {print f " line too long"; print}
+    '
 exec find $(find . -name src -type d) -type f ! -name \*~ | xargs awk -f $(dirname $0)/extra.awk
