87358896ff5c98ec0f4827dbd5b8a6732d49cff8
Julian Hyde <jhyde@apache.org>
2019-07-19 18:13:23 -0700

    Add linux support to rsync-camera.sh and te

--- tools/rsync-camera.sh
+++ tools/rsync-camera.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-rsync -arP /Volumes/Untitled/DCIM marmite:web/pix/staging/j6d
-ssh marmite "fiximg.sh --prefix j6d /home/jhyde/web/pix/staging/j6d"
+case $(uname -s) in
+(Linux) BASE='/media/jhyde/9C33-6BBD'; DEST=${HOME}/;;
+(*) BASE='/Volumes/Untitled'; DEST='marmite:';;
+esac
+
+rsync -arP --filter='- ._*.JPG' ${BASE}/DCIM ${DEST}web/pix/staging/j6d
+
+case $(uname -n) in
+(marmite) fiximg.sh --prefix j6d /home/jhyde/web/pix/staging/j6d;;
+(*) ssh marmite "fiximg.sh --prefix j6d /home/jhyde/web/pix/staging/j6d";;
+esac
 
 # End
 
